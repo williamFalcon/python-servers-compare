b51df7489106b27f1714a0d4bfc56b0cfcdb7c10
William Falcon <waf2107@columbia.edu>
2017-03-04 16:11:27 -0500

    added aws config

--- docker-flask-nginx-uwsgi-miniconda-3.4/app/application.py
+++ docker-flask-nginx-uwsgi-miniconda-3.4/app/application.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, Response, request
+import json
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
+def status():
+    data = {'status': 'ok - uwsgi'}
+    js = json.dumps(data)
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/regress', methods=['POST'])
 def hello():
-    return 'Hello test 2'
+
+    data = request.get_json()
+    Y = np.asarray(data['Y_train'])
+    X = np.asarray(data['X_train'])
+    X_test = np.asarray(data['X_test'])
+    X = np.insert(X, 0, 1.0, axis=1)
+    X_test = np.insert(X_test, 0, 1.0, axis=1)
+    W = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, Y))
+    Y_hat = list(np.dot(X_test, W).flatten())
+
+    resp = Response(json.dumps({'Y_hat':Y_hat}), status=200, mimetype='application/json')
+    return resp
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- docker-tornado/app/app.py
+++ docker-tornado/app/app.py
@@ -1,48 +1,42 @@
-from tornado import websocket, web, ioloop
+from tornado import web, ioloop
 import json
+import numpy as np
 
-cl = []
 
 class IndexHandler(web.RequestHandler):
     def get(self):
-        self.write('hello world')
+        self.write(json.dumps({'status': 'ok - tornado'}))
 
-class SocketHandler(websocket.WebSocketHandler):
-    def check_origin(self, origin):
-        return True
-
-    def open(self):
-        if self not in cl:
-            cl.append(self)
-
-    def on_close(self):
-        if self in cl:
-            cl.remove(self)
 
 class ApiHandler(web.RequestHandler):
 
     @web.asynchronous
-    def get(self, *args):
-        self.finish()
-        id = self.get_argument("id")
-        value = self.get_argument("value")
-        data = {"id": id, "value" : value}
-        data = json.dumps(data)
-        for c in cl:
-            c.write_message(data)
+    def post(self, *args):
+        print('received')
+        # # finish this response
+        # self.finish()
 
-    @web.asynchronous
-    def post(self):
-        pass
+        # regress
+        data = json.loads(self.request.body.decode('utf-8'))
+        Y = np.asarray(data['Y_train'])
+        X = np.asarray(data['X_train'])
+        X_test = np.asarray(data['X_test'])
+        X = np.insert(X, 0, 1.0, axis=1)
+        X_test = np.insert(X_test, 0, 1.0, axis=1)
+        W = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, Y))
+        Y_hat = list(np.dot(X_test, W).flatten())
+
+        self.write(json.dumps({'Y_hat':Y_hat}))
+
+        # # finish this response
+        self.finish()
 
 app = web.Application([
     (r'/', IndexHandler),
-    (r'/ws', SocketHandler),
-    (r'/api', ApiHandler),
-    (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
-    (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
+    (r'/regress', ApiHandler)
 ])
 
 if __name__ == '__main__':
+    print('server listening on port 80')
     app.listen(80)
     ioloop.IOLoop.instance().start()
